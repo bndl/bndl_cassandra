03eb68737751e9b8690a8cfedc90707f1cf16333
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-09-02 11:59:06 +0200

    add test for span by

--- bndl_cassandra/tests/test_read.py
+++ bndl_cassandra/tests/test_read.py
@@ -2,30 +2,32 @@ from bndl_cassandra.tests import CassandraTest
 
 
 class ReadTest(CassandraTest):
-    rows = [dict(key=str(i), cluster=str(i), varint_val=i) for i in range(100)]
+    key_count = 10
+    row_count = 100
+    rows = [dict(key=str(i % key_count), cluster=str(i), varint_val=i) for i in range(row_count)]
 
     def setUp(self):
         super().setUp()
         self.ctx.collection(self.rows).cassandra_save(self.keyspace, self.table).execute()
 
     def test_count(self):
-        self.assertEqual(self.ctx.cassandra_table(self.keyspace, self.table).count(push_down=False), len(self.rows))
-        self.assertEqual(self.ctx.cassandra_table(self.keyspace, self.table).count(push_down=True), len(self.rows))
+        self.assertEqual(self.ctx.cassandra_table(self.keyspace, self.table).count(push_down=False), self.row_count)
+        self.assertEqual(self.ctx.cassandra_table(self.keyspace, self.table).count(push_down=True), self.row_count)
 
     def test_cache(self):
         dset = self.ctx.cassandra_table(self.keyspace, self.table)
-        self.assertEqual(dset.count(), len(self.rows))  # count before
-        self.assertEqual(dset.cache().count(), len(self.rows))  # count while caching
-        self.assertEqual(dset.count(), len(self.rows))  # count after
+        self.assertEqual(dset.count(), self.row_count)  # count before
+        self.assertEqual(dset.cache().count(), self.row_count)  # count while caching
+        self.assertEqual(dset.count(), self.row_count)  # count after
 
     def test_collect_dicts(self):
         dicts = self.ctx.cassandra_table(self.keyspace, self.table).as_dicts()
-        self.assertEqual(len(dicts.collect()), len(self.rows))
+        self.assertEqual(len(dicts.collect()), self.row_count)
         self.assertEqual(type(dicts.first()), dict)
 
     def test_collect_tuples(self):
         tuples = self.ctx.cassandra_table(self.keyspace, self.table).as_tuples()
-        self.assertEqual(len(tuples.collect()), len(self.rows))
+        self.assertEqual(len(tuples.collect()), self.row_count)
         self.assertEqual(type(tuples.first()), tuple)
 
     def test_collect_dataframe(self):
@@ -34,9 +36,29 @@ class ReadTest(CassandraTest):
         except ImportError:
             self.skipTest('Pandas not installed')
         df = self.ctx.cassandra_table(self.keyspace, self.table).as_dataframe()
-        self.assertEqual(len(df.collect()), len(self.rows))
+        self.assertEqual(len(df.collect()), self.row_count)
         self.assertIsInstance(df.first(), tuple)
         self.assertIsInstance(df.take(3), pd.DataFrame)
+
+    def test_span_by(self):
+        try:
+            import pandas as pd
+        except ImportError:
+            self.skipTest('Pandas not installed')
+        df = self.ctx.cassandra_table(self.keyspace, self.table).span_by()
+        self.assertEqual(df.count(), self.row_count)
+        first = df.first()
+        self.assertIsInstance(first, tuple)
+        self.assertEqual(len(first), 2)
+        self.assertEqual(first[0], '0')
+        self.assertIsInstance(first[1], pd.DataFrame)
+        self.assertEqual(len(first[1]), self.row_count // self.key_count)
+        take3 = df.take(3)
+        self.assertIsInstance(take3, list)
+        self.assertEqual(len(take3), 3)
+        collected = df.collect()
+        self.assertIsInstance(collected, list)
+        self.assertEqual(len(collected), self.key_count)
 
     # TODO def test_select(self):
     # TODO def test_where(self):
