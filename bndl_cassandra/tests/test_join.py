7a638aa2e06c95f059890368d44b940fbb1e57a2
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-09-16 12:58:56 +0200

    added multijoin test

--- bndl_cassandra/tests/test_join.py
+++ bndl_cassandra/tests/test_join.py
@@ -16,6 +16,7 @@ class JoinTest(CassandraTest):
                                 .on(key=['key', 'cluster'])
                                 .as_dicts())
 
+
     def test_join_on_primary_kv_tuples(self):
         join = self.join_tuples
         self.assertEqual(join.count(), row_count)
@@ -24,11 +25,13 @@ class JoinTest(CassandraTest):
             self.assertEqual(cluster, right.cluster)
             self.assertEqual(varint_val, right.varint_val)
 
+
     def test_join_on_primary_dicts(self):
         join = self.join_dicts
         self.assertEqual(join.count(), row_count)
         for left, right in join.collect():
             self.assertEqual(left['varint_val'], right['varint_val'])
+
 
     def test_join_on_partition(self):
         left = self.ctx.collection(rows)
@@ -39,8 +42,22 @@ class JoinTest(CassandraTest):
             for row in right:
                 self.assertEqual(left['key'], row['key'])
 
+
     def test_join_select(self):
         join = self.join_dicts.select('varint_val').collect()
         for left, right in join:
             self.assertEqual(list(right.keys()), ['varint_val'])
             self.assertEqual(left['varint_val'], right['varint_val'])
+
+
+    def test_multijoin(self):
+        join = (self.join_dicts
+                    .select('varint_val')
+                    .starmap(lambda left, right: ((left['key'], left['cluster']), right))
+                    .join_with_cassandra(self.keyspace, self.table)
+                    .as_dicts()
+                    .select('varint_val'))
+
+        for ((key, cluster), second), third in join.collect():
+            self.assertEqual(int(cluster), second['varint_val'])
+            self.assertEqual(second['varint_val'], third['varint_val'])
