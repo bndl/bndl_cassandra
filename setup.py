5eeb30421b13d011fd98a547a6ea9e2d29984a8b
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-10-05 14:34:02 +0200

    bump version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ except ImportError:
 
 setup(
     name='bndl_cassandra',
-    version='0.3.3',
+    version='0.3.4',
     url='https://stash.tgho.nl/projects/THCLUSTER/repos/bndl_cassandra/browse',
     description='Read from and write to Apache Cassandra with BNDL',
     long_description=open('README.md').read(),
